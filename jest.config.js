e9ad5aeaae203a4750cb6cadb76e1840845815c1
ktscates <cates2506@gmail.com>
2025-02-11 18:20:55 +0200

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+  testEnvironment: "jest-environment-jsdom",
+};
