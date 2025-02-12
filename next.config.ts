358e35ee2875b21440c60889a228972d096686d7
ktscates <cates2506@gmail.com>
2025-02-12 22:39:33 +0200

    Fixing netlify deployemeny

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enables static export
+  trailingSlash: true, // Ensures proper file paths in Netlify
 };
 
 export default nextConfig;
